7a5706691cf8287f084ff364acfdc9670ac781db
Paula Nicolau <pnicolau@maclab33.local>
2016-06-20 15:10:51 -0500

    Allow for entries to be re-ordered

--- CollegeProfileBuilder/ViewController.swift
+++ CollegeProfileBuilder/ViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
   
+    @IBOutlet var editButton: UIBarButtonItem!
 
     @IBOutlet var tableView: UITableView!
     
     var colleges = ["University of Illinois", "University of Chicago", "NorthWestern University", "Loyola University", "DePaul University"]
     override func viewDidLoad()
     {
+        editButton.tag = 0
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -44,7 +46,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBAction func onTappedButton(sender: UIBarButtonItem)
     {
         let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
-        alert.addTextFieldWithConfigurationHandler { (textField) in
+        alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
             textField.placeholder = "Add College Here"
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
@@ -56,11 +58,31 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
         alert.addAction(addAction)
         
-        self.presentViewController(alert, animated: true,completion: nil)
-        
-        
+        self.presentViewController(alert, animated: true, completion: nil)
         
     }
+    
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        let college = colleges[sourceIndexPath.row]
+        colleges.removeAtIndex(sourceIndexPath.row)
+        colleges.insert(college, atIndex: destinationIndexPath.row)
+    }
+    
+    @IBAction func onTappedEditButton(sender: UIBarButtonItem) {
+        if sender.tag == 0
+        { tableView.editing = true
+            sender.tag = 1
+        }
+        else {
+            tableView.editing = false
+            sender.tag = 0
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
